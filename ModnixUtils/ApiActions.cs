febc06f4bd566f0c8037b56d38e2d68188075437
Tahvohck <Tahvohck@gmail.com>
2020-04-21 09:13:16 -0400

    Modnix Utils MVP

---
+++ ModnixUtils/ApiActions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModnixUtils
+{
+    /// <summary>
+    /// Actions for use in API function
+    /// </summary>
+    public class ModnixAPIActions
+    {
+        /// <summary>
+        /// <para>Spec: Name of API to add</para>
+        /// <para>Param: API Function</para>
+        /// <para>Return: True on success</para>
+        /// </summary>
+        public const string api_add     = "api_add";
+        public const string api_info    = "api_info";
+        public const string api_list    = "api_list";
+        public const string api_remove  = "api_remove";
+        public const string assembly    = "assembly";
+        public const string assemblies  = "assemblies";
+        /// <summary>
+        /// Work with the configuration.
+        /// <para>Spec (default, ""): Returns deserialised object of same type</para>
+        /// <para>Spec ("save", "write"): Save the provided object to the mod's config file.</para>
+        /// </summary>
+        public const string config      = "config";
+        public const string dir         = "dir";
+        public const string logger      = "logger";
+        public const string mod_info    = "mod_info";
+        public const string mod_list    = "mod_list";
+        public const string path        = "path";
+        public const string stacktrace  = "stacktrace";
+        public const string version     = "version";
+
+        /// <summary>
+        /// Set of Log actions
+        /// </summary>
+        public class log
+        {
+            public const string normal      = "log";
+            public const string critical    = "log crit";
+            public const string error       = "log error";
+            public const string warning     = "log warn";
+            public const string info        = "log info";
+            public const string verbose     = "log verbose";
+            /// <summary>
+            /// Special action that immediately flushes all queued messages to the log
+            /// </summary>
+            public const string flush       = "log flush";
+        }
+    }
+}
---
+++ ModnixUtils/BasicUtil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ModnixUtils
+{
+    public class BasicUtil
+    {
+        public static void Log(object input, Func<string, object, object> api = null)
+        {
+            if (api is null) return;
+            api(ModnixAPIActions.log.info, input);
+        }
+    }
+}
