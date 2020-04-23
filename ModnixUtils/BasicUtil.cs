91662e59f80f9b0945361202066b1ce034da8594
Tahvohck <Tahvohck@gmail.com>
2020-04-23 16:11:32 -0400

    BasicUtil.Log now requires api as it's useless otherwise

--- ModnixUtils/BasicUtil.cs
+++ ModnixUtils/BasicUtil.cs
@@ -5,7 +5,7 @@ namespace ModnixUtils
 {
     public class BasicUtil
     {
-        public static void Log(object input, Func<string, object, object> api = null)
+        public static void Log(object input, Func<string, object, object> api)
         {
             if (api is null) return;
             api(ModnixAPIActions.log.info, input);
