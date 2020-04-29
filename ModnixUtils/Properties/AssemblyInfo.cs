2aceae4406ff7b792d919e9212d09b619735c33c
Tahvohck <Tahvohck@gmail.com>
2020-04-29 03:01:31 -0400

    Added T4 template for versioning.

--- ModnixUtils/Properties/AssemblyInfo.cs
+++ ModnixUtils/Properties/AssemblyInfo.cs
@@ -6,11 +6,15 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ModnixUtils")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Basic Utilities for Modnix/Phoenix Point Modding.")]
+#if DEBUG
+    [assembly: AssemblyConfiguration("DEBUG")]
+#else
+    [assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Tahvohck")]
 [assembly: AssemblyProduct("ModnixUtils")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©Tahvohck  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +25,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6866a0c1-1fd4-44fb-a3b2-b782cb26e3b8")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
