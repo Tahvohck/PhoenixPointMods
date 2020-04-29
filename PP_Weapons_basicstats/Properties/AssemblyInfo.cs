32fc115e9ac8a76f104b737d01b8462972605d3e
Tahvohck <Tahvohck@gmail.com>
2020-04-29 03:41:08 -0400

    Added unified versioning configuration (based on T4)

--- PP_Weapons_basicstats/Properties/AssemblyInfo.cs
+++ PP_Weapons_basicstats/Properties/AssemblyInfo.cs
@@ -26,16 +26,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9926e89f-6652-4511-afd9-8a6b40acb88e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
