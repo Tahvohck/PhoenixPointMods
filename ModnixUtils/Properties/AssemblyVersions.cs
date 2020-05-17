71ca625f0405ea3b5b03471c1f349a54bc799f9c
Tahvohck <Tahvohck@gmail.com>
2020-05-16 21:23:19 -0400

    Version bump

--- ModnixUtils/Properties/AssemblyVersions.cs
+++ ModnixUtils/Properties/AssemblyVersions.cs
@@ -6,8 +6,8 @@
 //      Minor Version
 //      Build Number
 //      Revision (Days since start of year)
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0.129")]
+[assembly: AssemblyVersion("1.3.1")]
+[assembly: AssemblyFileVersion("1.3.1.137")]
 [assembly: AssemblyInformationalVersion("1.3" +
 #if DEBUG
     " DEBUG")]
